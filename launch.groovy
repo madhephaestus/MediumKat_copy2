4544fa3b8439f1e6772caf97429b9fdac2501f0f
Kevin Harrington <mad.hephaestus@gmail.com>
2019-12-29 15:46:27 -0500

    new Robot content

---
+++ launch.groovy
@@ -0,0 +1,4 @@
+ScriptingEngine.gitScriptRun(	"https://github.com/OperationSmallKat/SmallKat_V2.git", 
+								"loadRobot.groovy", 
+["https://github.com/madhephaestus/MediumKat_copy2.git",
+		"MediumKat_copy2.xml","GameController_22","hidDevice","MediumKat_copy2"]);
